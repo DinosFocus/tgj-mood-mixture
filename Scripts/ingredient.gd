3aac790356b9f53a0563ad4ec66f26dc9fdf45e1
nathanBarloy <nbarloy@gmail.com>
2024-08-17 11:16:37 +0200

    Ajout de l'objet "Ingredient"

---
+++ Scripts/ingredient.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var color = Color(1,0,0)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# donne une couleur à l'ingrédient
+func set_color(col: Color) -> void:
+	color = col
+	$Liquid.color = color
+	
