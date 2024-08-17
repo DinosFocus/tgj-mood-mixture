b332630889394fbb24382f3ca4e57108da7d3315
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2024-08-17 22:15:41 +0200

    Ajout de différents niveaux

--- Scripts/potion_maker.gd
+++ Scripts/potion_maker.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
+@export var target_color = Color.BLACK
+@export var max_distance = .5
+@export var min_quantity = .5
+
 var fill_speed = .25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$Potion.target_color = target_color
+	$Potion.max_distance = max_distance
+	$Potion.min_quantity = min_quantity
 	update_buttons()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	for ingredient_button in [$CyanIngredientButton, $YellowIngredientButton, $MagentaIngredientButton, $BlackIngredientButton]:
+	for ingredient_button in [$CyanIngredientButton, $MagentaIngredientButton, $YellowIngredientButton, $BlackIngredientButton]:
 		if ingredient_button.button_pressed:
 			$Potion.add_liquid(delta * fill_speed, ingredient_button.color)
 			update_buttons()
