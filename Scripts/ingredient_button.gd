2f9d00a1b3542f5da9566740a5a43239ec64d304
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2024-08-25 19:22:51 +0200

    Remplacement des PNGs des récipients par des SVGs

--- Scripts/ingredient_button.gd
+++ Scripts/ingredient_button.gd
@@ -6,12 +6,12 @@ var mouse_over
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Ingridient.set_color(color)
+	$Ingredient.set_color(color)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Ingridient.set_alpha(.8 if mouse_over && !disabled else .2)
+	$Ingredient.set_alpha(.8 if mouse_over && !disabled else .2)
 
 
 func _on_mouse_entered() -> void:
